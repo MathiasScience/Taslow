346ac61dec25c20f348166a094607aa2debe7da8
Mathias Science <mathiasgamern1@gmail.com>
2026-04-07 19:40:25 -0500

    Añadi una lista de tareas hechas

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,6 +1,7 @@
 const input = document.getElementById('miInput');
 const boton = document.getElementById('btnEnviar');
 const lista = document.getElementById('miLista');
+const listaHecha = document.getElementById('listaHecha');
 const btnModo = document.getElementById('btnModo');
 
 boton.addEventListener('click', function() {
@@ -14,15 +15,23 @@ boton.addEventListener('click', function() {
         // 2. Crear el botón de eliminar
         const btnEliminar = document.createElement('button');
         btnEliminar.textContent = "Eliminar";
-        
-        // 3. Programar qué pasa al hacer clic en "Eliminar"
-        btnEliminar.onclick = function() {
-            li.remove(); // Borra el elemento li completo
-        };
 
-        // 4. Meter el botón dentro del li, y el li dentro de la lista
+        // 3. Meter el botón dentro del li, y el li dentro de la lista
         li.appendChild(btnEliminar);
         lista.appendChild(li);
+
+		// 4. Programar qué pasa al hacer clic en "Eliminar"
+		btnEliminar.onclick = function() {
+            if (li.parentNode === lista) {
+                // Si está en la lista de pendientes, la movemos a la de hechas
+                listaHecha.appendChild(li);
+                btnEliminar.textContent = "Deshacer"; // Cambiamos el texto en lugar de borrar el botón
+            } else {
+                // Si ya estaba en hecha, la regresamos a pendientes
+                lista.appendChild(li);
+                btnEliminar.textContent = "Eliminar";
+            }
+        };
 
         input.value = "";
     }
